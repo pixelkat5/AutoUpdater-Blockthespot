e36b699f2d2d572de1898c8160a51315c5701953
Yolilad <152070897+Yolilad@users.noreply.github.com>
2023-11-29 10:55:33 +0300

    Update Modify.cpp (#501)
    * Update Modify.cpp
    
    * Update Modify.cpp

--- src/Modify.cpp
+++ src/Modify.cpp
@@ -237,10 +237,10 @@ int cef_zip_reader_read_file_hook(void* self, void* buffer, size_t bufferSize)
 			Logger::Log(L"sp://ads/v1/ads/ - failed not found!", Logger::LogLevel::Error);
 		}
 
-		const auto premium_free = PatternScanner::ScanFirst(reinterpret_cast<std::size_t>(buffer), bufferSize, L"\"free\"===e.session?.productState?.catalogue?.toLowerCase(),r=e=>null!==e.session?.productState&&1===parseInt(e.session?.productState?.ads,10),o=e=>\"premium\"===e.session?.productState?.catalogue?.toLowerCase(),");
+		const auto premium_free = PatternScanner::ScanFirst(reinterpret_cast<std::size_t>(buffer), bufferSize, L"\"free\"===e.session?.productState?.catalogue?.toLowerCase(),r=e=>\"premium\"===e.session?.productState?.catalogue?.toLowerCase()");
 		if (premium_free.is_found()) {
 			//Print(L"{}", premium_free.read<const char*>());
-			if (premium_free.write<const char*>("\"premium\"===e.session?.productState?.catalogue?.toLowerCase(),r=e=>null!==e.session?.productState&&1===parseInt(e.session?.productState?.ads,10),o=e=>\"free\"===e.session?.productState?.catalogue?.toLowerCase(),")) {
+			if (premium_free.write<const char*>("\"premium\"===e.session?.productState?.catalogue?.toLowerCase(),r=e=>\"free\"===e.session?.productState?.catalogue?.toLowerCase()")) {
 				Logger::Log(L"premium patched!", Logger::LogLevel::Info);
 			}
 			else {
