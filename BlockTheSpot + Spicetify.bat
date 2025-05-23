5128294098ceef230fce4a40bbabcbd632a8198a
Pixel <pixelsworkemail@gmail.com>
2025-05-22 19:26:43 -0500

    Update BlockTheSpot + Spicetify.bat

--- BlockTheSpot + Spicetify.bat
+++ BlockTheSpot + Spicetify.bat
@@ -1,13 +1,7 @@
 @echo off
 
-set /p UserInput= "Spicetify will be Installed. If you don't agree, use the BlockTheSpot script, else press 'y' to continue (y/n)? "
-if /i "%UserInput%"=="y" (
-    powershell -Command "& {iwr -useb https://raw.githubusercontent.com/spicetify/spicetify-cli/master/install.ps1 | iex}"
-    powershell -Command "& {iwr -useb https://raw.githubusercontent.com/spicetify/spicetify-marketplace/main/resources/install.ps1 | iex}"
-    powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/mrpond/BlockTheSpot/master/install.ps1' | Invoke-Expression}"
-    pause
-) else (
-    echo "Patch not installed."
-    pause
-    exit /b
-)
+powershell -Command "& {iwr -useb https://raw.githubusercontent.com/spicetify/spicetify-cli/master/install.ps1 | iex}"
+powershell -Command "& {iwr -useb https://raw.githubusercontent.com/spicetify/spicetify-marketplace/main/resources/install.ps1 | iex}"
+powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/mrpond/BlockTheSpot/master/install.ps1' | Invoke-Expression}"
+pause
+exit /b
