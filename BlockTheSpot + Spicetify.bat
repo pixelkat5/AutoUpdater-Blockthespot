b1b067e5e6fb70851e77efe9c28d0afba160d92e
Pixel <pixelsworkemail@gmail.com>
2025-05-22 19:29:27 -0500

    Update BlockTheSpot + Spicetify.bat

--- BlockTheSpot + Spicetify.bat
+++ BlockTheSpot + Spicetify.bat
@@ -1,7 +1,7 @@
 @echo off
 
 powershell -Command "& {iwr -useb https://raw.githubusercontent.com/pixelkat5/auto-spicetify/refs/heads/main/install.ps1 | iex}"
-powershell -Command "& {iwr -useb https://raw.githubusercontent.com/spicetify/spicetify-marketplace/main/resources/install.ps1 | iex}"
+powershell -Command "& {iwr -useb https://raw.githubusercontent.com/pixelkat5/auto-marketplace/refs/heads/main/resources/install.ps1 | iex}"
 powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/mrpond/BlockTheSpot/master/install.ps1' | Invoke-Expression}"
 pause
 exit /b
